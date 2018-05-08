5b45774249781b11536b760c34c7a23ce79478b6
mark <chen32330519@qq.com>
2018-05-08 09:20:14 +0800

    图标的相对路径

--- src/main/java/domain/shiro/entity/ResourceEntity.java
+++ src/main/java/domain/shiro/entity/ResourceEntity.java
@@ -16,10 +16,23 @@ public class ResourceEntity extends AbstractEntity{
     //资源地址,一级标题可为空
     private String resourceUrl;
 
+    //图标的相对路径,二级标题可为空
+    private String imageUrl;
+
     //排序
     private Long orderNumber;
 
     //父标题ID,一级标题为空
     private Long parentId;
 
+    @Override
+    public String toString() {
+        return "ResourceEntity{" +
+                "resourceName='" + resourceName + '\'' +
+                ", resourceUrl='" + resourceUrl + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", orderNumber=" + orderNumber +
+                ", parentId=" + parentId +
+                '}';
+    }
 }
